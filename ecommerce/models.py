07d182cccf1e5ccab8fd6ac9a247a7ffa4eec538
Nidhin <nidhintsajee@gmail.com>
2016-12-17 15:56:28 +0530

    My e-commerce site app

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 import os.path
-from PIL import Image
+import Image
 from cStringIO import StringIO
 from django.core.files.uploadedfile import SimpleUploadedFile
 
