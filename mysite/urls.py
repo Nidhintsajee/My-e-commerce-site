2014bd92d20dc6abe8bb5a92b609b43929d61324
Nidhin <nidhintsajee@gmail.com>
2016-12-17 18:22:28 +0530

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.views.generic.base import TemplateView
-from ecommerce import views
+from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
