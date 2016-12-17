7058508c032f1c5bd2aefd75f2e2779a6364e3aa
Nidhin <nidhintsajee@gmail.com>
2016-12-17 18:06:17 +0530

    e

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -4,19 +4,19 @@ from django.shortcuts import render, get_object_or_404 , redirect
 from .forms import ProductForm
 
 def list_products(request):
-    List = Product.objects.all()
-    context = {'List':List}
-    return render(request, 'ecommerce/list_products.html',context)
+	List = Product.objects.all()
+    	context = {'List':List}
+    	return render(request, 'ecommerce/list_products.html',context)
 
 def detail_product(request, pk):
-    obj = Product.objects.get(id=pk)
-    print(obj)
-    return  render(request,'detail_product.html', {'obj':obj})
+    	obj = Product.objects.get(id=pk)
+	print(obj)
+    	return  render(request,'detail_product.html', {'obj':obj})
 
 def add_product(request):
-    form = ProductForm()
-    context = {'form':form}
-    return render(request, 'ecommerce/add_product.html', context)	
+    	form = ProductForm()
+    	context = {'form':form}
+    	return render(request, 'ecommerce/add_product.html', context)	
 
 def save_product(request):
 	if request.method == "POST":	
